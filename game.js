f20e9622a88825575a658179be4d795578296d79
Sai Jilla <96095408+sai3000pro@users.noreply.github.com>
2023-09-01 15:35:13 -0400

    Added functionality

--- game.js
+++ game.js
@@ -1,26 +1,41 @@
+
 let buttonColours = ["red", "blue", "green", "yellow"];
+
 let gamePattern = [];
-// var blue = new Audio('./sounds/blue.mp3');
-// var green = new Audio('./sounds/green.mp3');
-// var red = new Audio('./sounds/red.mp3');
-// var yellow = new Audio('./sounds/yellow.mp3');
-// var wrong = new Audio('./sounds/wrong.mp3');
 let userClickedPattern = [];
-var level = 0;
+
+
 var gameStart = false;
+
+
+var level = 0;
 
 $(document).keypress(function() {
     if (!gameStart) {
-        gameStart = true;
+        $("#level-title").text("Level " + level);
         nextSequence();
+        gameStart = true;
     }
-})
+});
+
+
+
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
 
 function nextSequence() {
     level++;
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColour = buttonColors[randomNumber];
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -28,21 +43,32 @@ function nextSequence() {
     playSound(randomChosenColour);
 }
 
-$(".btn").click(function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-})
+
 
 function animatePress(currentColour) {
-    $("#" + currentColour).addClass(".pressed");
+    $("#" + currentColour).addClass("pressed");
     setTimeout(function() {
-        $("#" + currentColour).removeClass(".pressed");
+        $("#" + currentColour).removeClass("pressed");
     }, 100)
 }
 
 function playSound(name) {
     var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+        console.log("Success!");
+    }
+    else {
+        console.log("Fail!");
+    }
+
+    if (gamePattern.length - 1 == currentLevel) {
+        setTimeout(function() {
+            userClickedPattern=[];
+            nextSequence();
+        }, 1000)
+    }
 }
