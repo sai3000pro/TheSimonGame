2b9e44896ab478916c5618afd678a2905d33809a
Sai Jilla <96095408+sai3000pro@users.noreply.github.com>
2023-08-31 22:45:04 -0400

    Added some document functionality - game starts on keypress

--- game.js
+++ game.js
@@ -1,27 +1,48 @@
-var randomNumber;
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-var randomChosenColour;
 // var blue = new Audio('./sounds/blue.mp3');
 // var green = new Audio('./sounds/green.mp3');
 // var red = new Audio('./sounds/red.mp3');
 // var yellow = new Audio('./sounds/yellow.mp3');
 // var wrong = new Audio('./sounds/wrong.mp3');
-var userChosenColour;
 let userClickedPattern = [];
+var level = 0;
+var gameStart = false;
 
-function newSequence() {
-    randomNumber = Math.floor(Math.random()*4);
-    randomChosenColor = buttonColors[randomNumber];
+$(document).keypress(function() {
+    if (!gameStart) {
+        gameStart = true;
+        nextSequence();
+    }
+})
+
+function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     // randomChosenColour.play();
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
 
 $(".btn").click(function() {
-    userChosenColour = $(this).attr("id");
+    var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass(".pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass(".pressed");
+    }, 100)
+}
+
+function playSound(name) {
+    var audio = new Audio("./sounds/" + name + ".mp3");
+    audio.play();
+}
